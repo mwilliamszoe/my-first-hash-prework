2c8743c959fc3decde5f1c7513d58513a08e9cae
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 21:34:33 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {"key1" => "value1"}
+  
 
   
 end
@@ -28,16 +30,17 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
-
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  
 
   # return the shipping_manifest hash below
+  shipping_manifest
 
 end
